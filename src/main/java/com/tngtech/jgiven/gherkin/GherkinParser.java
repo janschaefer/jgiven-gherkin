ed3bc4cf6b2454dbe55d5b55c823f1eef2372abe
Jan Schäfer <jan.schaefer@tngtech.com>
2015-07-28 22:08:17 +0200

    handle arguments

--- src/main/java/com/tngtech/jgiven/gherkin/GherkinParser.java
+++ src/main/java/com/tngtech/jgiven/gherkin/GherkinParser.java
@@ -1,9 +1,15 @@
 package com.tngtech.jgiven.gherkin;
 
 import java.io.*;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import com.google.common.base.CaseFormat;
 import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import com.google.common.io.CharSource;
 import com.google.common.io.CharStreams;
 import com.google.common.io.Files;
@@ -16,6 +22,7 @@ import gherkin.ast.Step;
 public class GherkinParser {
     private static final String INDENT = "    ";
     private static final String INDENT_2 = Strings.repeat( INDENT, 2 );
+    public static final String NUMBER_REGEX = "[0-9]+[.,]?[0-9]*";
 
     public static void main( String... args ) throws IOException {
         Parser<Feature> parser = new Parser<Feature>();
@@ -33,6 +40,9 @@ public class GherkinParser {
     }
 
     private static void generateJGivenCode( Feature feature, PrintWriter writer ) throws IOException {
+        writer.println( "import org.junit.Test;" );
+        writer.println( "import com.tngtech.jgiven.junit.ScenarioTest;" );
+
         writer.println( "public class " + toCamelCase( feature.getDescription() ) + "Test extends " );
         writer.println( INDENT_2 + "ScenarioTest<GivenState, WhenAction, ThenOutcome> { " );
 
@@ -46,6 +56,11 @@ public class GherkinParser {
     private static void generateScenario( ScenarioDefinition scenarioDefinition, PrintWriter writer ) {
         writer.println();
         writer.println( INDENT + "@Test" );
+
+        if( containsNonJavaChars( scenarioDefinition.getDescription() ) ) {
+            writer.println( INDENT + "@Description(\"" + javaStringEscape( scenarioDefinition.getDescription() ) + "\")" );
+        }
+
         writer.println( INDENT + "public void " + toSnakeCase( scenarioDefinition.getName() ) + " {" );
 
         boolean first = true;
@@ -71,16 +86,42 @@ public class GherkinParser {
         }
 
         writer.print( INDENT_2 + keyword + "()." );
-        writer.print( toSnakeCase( step.getText() ) + "()" );
+        writer.print( toStepMethodCall( step.getText() ) );
 
+    }
+
+    private static String toStepMethodCall( String step ) {
+        Pattern numberPattern = Pattern.compile( NUMBER_REGEX );
+
+        List<String> args = Lists.newArrayList();
+        Matcher matcher = numberPattern.matcher( step );
+        while( matcher.find() ) {
+            args.add( matcher.group() );
+        }
+
+        matcher.reset();
+        return toSnakeCase( matcher.replaceAll( "\\$" ) ) + "(" +
+                Joiner.on( ", " ).join( args ) + ")";
+
+    }
+
+    private static String javaStringEscape( String string ) {
+        return string.replace( "\\", "\\\\" );
+    }
+
+    private static boolean containsNonJavaChars( String string ) {
+        if( string == null ) {
+            return false;
+        }
+        return !Pattern.matches( "^[_a-zA-Z][0-9_a-zA-Z]*$", string );
     }
 
     private static String toSnakeCase( String name ) {
-        return name.replace( ' ', '_' );
+        return name.replaceAll( "[^A-Za-z0-9$]", "_" );
     }
 
     private static String toCamelCase( String name ) {
-        return name.replace( " ", "" ).replaceAll( "[^A-Za-z]", "_" );
+        return CaseFormat.LOWER_UNDERSCORE.to( CaseFormat.UPPER_CAMEL, toSnakeCase( name ) );
     }
 
 }
